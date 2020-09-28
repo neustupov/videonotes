9151d0ec1b8e90529aa1042851daa32168f4c88b
Dmitry_Neustupov <neustupov@yandex.ru>
2020-09-28 17:21:36 +0300

    Initial commit

---
+++ src/main/java/org/neustupov/videonotes/VideoNotesApplication.java
@@ -0,0 +1,13 @@
+package org.neustupov.videonotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/neustupov/videonotes/VideoNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.neustupov.videonotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
